57b3b63f33ba258f185d4ac1cb2f365689033a84
Justin Braithwaite <justinbraithwaite@gmail.com>
2015-03-31 02:54:48 -0700

    No need for root instance

--- app/router.jsx
+++ app/router.jsx
@@ -3,20 +3,9 @@
 var React = require('react/addons');
 var ReactRouter = require('react-router');
 var routes = require('./routes.jsx');
-var rootInstance = null;
 
 var InitializeRouter = function(View, state) {
-  rootInstance = React.render(<View params={state.params}/>, document.getElementById('app-container'));
+  React.render(<View params={state.params}/>, document.getElementById('app-container'));
 };
-
-// https://github.com/gaearon/react-hot-loader/tree/master/docs#usage-with-external-react
-if (module.hot) {
-  require('react-hot-loader/Injection').RootInstanceProvider.injectProvider({
-    getRootInstances: function () {
-      // Help React Hot Loader figure out the root component instances on the page:
-      return [rootInstance];
-    }
-  });
-}
 
 ReactRouter.run(routes, ReactRouter.HistoryLocation, InitializeRouter);
